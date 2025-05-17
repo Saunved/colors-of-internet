bf9630ace5b1e68daf9a2f60a704d0aadb1f53a0
Saunved <saunved@gmail.com>
2025-05-17 13:49:07 +0200

    Memoize the Cell component

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,11 @@
-export default function Cell({ id, color, opacitySignal, cooldownSignal, onClick }) {
+import { memo } from "preact/compat";
+
+const Cell = ({ id, color, opacitySignal, cooldownSignal, onClick }) => {
+
+    if (id === 0) {
+        console.log(opacitySignal.value, cooldownSignal?.value);
+    }
+
     const opacity = opacitySignal.value;
     const cooldown = cooldownSignal?.value;
     const _color = color.replace(/#a#/g, opacity);
@@ -9,3 +16,9 @@ export default function Cell({ id, color, opacitySignal, cooldownSignal, onClick
         </button>
     );
 }
+
+export default memo(Cell, (prevProps, nextProps) => {
+    return prevProps.color === nextProps.color &&
+        prevProps.cooldownSignal.value === nextProps.cooldownSignal.value &&
+        prevProps.opacitySignal.value === nextProps.opacitySignal.value;
+});
